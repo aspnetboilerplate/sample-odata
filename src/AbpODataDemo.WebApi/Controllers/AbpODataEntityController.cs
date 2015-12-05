60f2e0c2382163c99cf8e3db1c755ecddbf91d61
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-12-05 23:50:13 +0200

    Re-formatted for #19.

--- src/AbpODataDemo.WebApi/Controllers/AbpODataEntityController.cs
+++ src/AbpODataDemo.WebApi/Controllers/AbpODataEntityController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,24 +13,26 @@ using Abp.Domain.Uow;
 
 namespace AbpODataDemo.Controllers
 {
-    public abstract class AbpODataEntityController<TEntity> : AbpODataEntityController<TEntity, int> 
+    public abstract class AbpODataEntityController<TEntity> : AbpODataEntityController<TEntity, int>
         where TEntity : class, IEntity<int>
     {
-        protected AbpODataEntityController(IRepository<TEntity> repository) : base(repository)
+        protected AbpODataEntityController(IRepository<TEntity> repository)
+            : base(repository)
         {
 
         }
     }
 
     public abstract class AbpODataEntityController<TEntity, TPrimaryKey> : ODataController
-    where TPrimaryKey: IEquatable<TPrimaryKey>
-    where TEntity : class, IEntity<TPrimaryKey>
+        where TPrimaryKey : IEquatable<TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
     {
         public IUnitOfWorkManager UnitOfWorkManager { get; set; }
 
         protected IRepository<TEntity, TPrimaryKey> Repository { get; private set; }
 
         private IUnitOfWorkCompleteHandle _unitOfWorkCompleteHandler;
+
         private bool _disposed;
 
         protected AbpODataEntityController(IRepository<TEntity, TPrimaryKey> repository)
@@ -41,7 +45,7 @@ namespace AbpODataDemo.Controllers
             _unitOfWorkCompleteHandler = UnitOfWorkManager.Begin();
             return base.ExecuteAsync(controllerContext, cancellationToken);
         }
-        
+
         [EnableQuery]
         public IQueryable<TEntity> Get()
         {
@@ -61,6 +65,7 @@ namespace AbpODataDemo.Controllers
             {
                 return BadRequest(ModelState);
             }
+
             var created = await Repository.InsertAsync(entity);
             return Created(created);
         }
@@ -71,11 +76,13 @@ namespace AbpODataDemo.Controllers
             {
                 return BadRequest(ModelState);
             }
+            
             var dbLookup = await Repository.GetAsync(key);
             if (entity == null)
             {
                 return NotFound();
             }
+            
             entity.Patch(dbLookup);
             return Updated(entity);
         }
@@ -86,10 +93,12 @@ namespace AbpODataDemo.Controllers
             {
                 return BadRequest(ModelState);
             }
+            
             if (!Equals(key, update.Id))
             {
                 return BadRequest();
             }
+            
             var updated = await Repository.UpdateAsync(update);
             return Updated(updated);
         }
@@ -101,10 +110,11 @@ namespace AbpODataDemo.Controllers
             {
                 return NotFound();
             }
+            
             await Repository.DeleteAsync(key);
             return StatusCode(HttpStatusCode.NoContent);
         }
-        
+
         protected override void Dispose(bool disposing)
         {
             if (!_disposed)
