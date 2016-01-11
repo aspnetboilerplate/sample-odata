e103492b475178bded3455f1f9d40f29abf01a2d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-01-11 17:23:51 +0200

    Added GetPhones to PersonsController.

--- src/AbpODataDemo.WebApi/Controllers/PersonsController.cs
+++ src/AbpODataDemo.WebApi/Controllers/PersonsController.cs
@@ -1,4 +1,6 @@
-﻿using Abp.Domain.Repositories;
+﻿using System.Linq;
+using System.Web.OData;
+using Abp.Domain.Repositories;
 using Abp.WebApi.OData.Controllers;
 using AbpODataDemo.People;
 
@@ -6,10 +8,23 @@ namespace AbpODataDemo.Controllers
 {
     public class PersonsController : AbpODataEntityController<Person>
     {
-        public PersonsController(IRepository<Person> repository)
-            : base(repository)
-        {
+        private readonly IRepository<Phone> _phoneRepository;
 
+        public PersonsController(
+            IRepository<Person> repository,
+            IRepository<Phone> phoneRepository)
+            : base(
+                repository)
+        {
+            _phoneRepository = phoneRepository;
+        }
+
+        //ex: http://localhost:61842/odata/Persons(1)/Phones
+        [EnableQuery]
+        public virtual IQueryable<Phone> GetPhones([FromODataUri] int key)
+        {
+            return _phoneRepository.GetAll().Where(ph => ph.PersonId == key);
+            //return Repository.Get(key).Phones.AsQueryable(); //Alternative, without need to phone repository.
         }
     }
 }
