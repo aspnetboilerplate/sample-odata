cd72a02cae5669d4e3db516f97b63d177f6522fc
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-07-20 14:02:59 +0300

    Upgraded to ABP v0.10.1.

--- src/AbpODataDemo.WebHost/Global.asax.cs
+++ src/AbpODataDemo.WebHost/Global.asax.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using Abp.Dependency;
 using Abp.Web;
 using Castle.Facilities.Logging;
 
 namespace AbpODataDemo.WebHost
 {
-    public class Global : AbpWebApplication
+    public class Global : AbpWebApplication<AbpODataDemoWebHostModule>
     {
         protected override void Application_Start(object sender, EventArgs e)
         {
-            IocManager.Instance.IocContainer.AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
+            AbpBootstrapper.IocManager
+                .IocContainer
+                .AddFacility<LoggingFacility>(f => f.UseLog4Net()
+                .WithConfig("log4net.config"));
+
             base.Application_Start(sender, e);
         }
     }
