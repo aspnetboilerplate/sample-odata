1d3c887f4d79befc26559f3e31c4b811ce935b65
Musa Demir <musa.demir@volosoft.com>
2019-09-02 17:00:08 +0300

    Upgared to ABP 4.8.0

--- AbpODataDemo-Core/aspnet-core/src/AbpODataDemo.Core/Authorization/Roles/RoleManager.cs
+++ AbpODataDemo-Core/aspnet-core/src/AbpODataDemo.Core/Authorization/Roles/RoleManager.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Abp.Authorization;
 using Abp.Authorization.Roles;
+using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
+using Abp.Organizations;
 using Abp.Runtime.Caching;
 using Abp.Zero.Configuration;
 using AbpODataDemo.Authorization.Users;
@@ -12,25 +14,7 @@ namespace AbpODataDemo.Authorization.Roles
 {
     public class RoleManager : AbpRoleManager<Role, User>
     {
-        public RoleManager(
-            RoleStore store, 
-            IEnumerable<IRoleValidator<Role>> roleValidators, 
-            ILookupNormalizer keyNormalizer, 
-            IdentityErrorDescriber errors, 
-            ILogger<AbpRoleManager<Role, User>> logger,
-            IPermissionManager permissionManager, 
-            ICacheManager cacheManager, 
-            IUnitOfWorkManager unitOfWorkManager,
-            IRoleManagementConfig roleManagementConfig)
-            : base(
-                  store,
-                  roleValidators, 
-                  keyNormalizer, 
-                  errors, logger, 
-                  permissionManager,
-                  cacheManager, 
-                  unitOfWorkManager,
-                  roleManagementConfig)
+        public RoleManager(AbpRoleStore<Role, User> store, IEnumerable<IRoleValidator<Role>> roleValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, ILogger<AbpRoleManager<Role, User>> logger, IPermissionManager permissionManager, ICacheManager cacheManager, IUnitOfWorkManager unitOfWorkManager, IRoleManagementConfig roleManagementConfig, IRepository<OrganizationUnit, long> organizationUnitRepository, IRepository<OrganizationUnitRole, long> organizationUnitRoleRepository) : base(store, roleValidators, keyNormalizer, errors, logger, permissionManager, cacheManager, unitOfWorkManager, roleManagementConfig, organizationUnitRepository, organizationUnitRoleRepository)
         {
         }
     }
